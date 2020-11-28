d69b2e3cc05b238bf033681d200454688bfd2c2c
psw <tmddnjs0404@hansung.ac.kr>
2020-11-28 14:28:49 +0000

    5th

--- HW.java
+++ HW.java
@@ -2,5 +2,6 @@ public class HW {
     public static void main(String[] args) {
         System.out.println("I Hate HW.");
         System.out.println("Maybe.");
+        System.out.println("Not Sure.");
     }
 }
